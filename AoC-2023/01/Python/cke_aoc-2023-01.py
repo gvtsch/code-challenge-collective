d818220c174183e1e0b5105d53399051d6f36b9c
gvtsch <kempkes.christoph@gmail.com>
2024-11-24 07:57:20 +0100

    Day 1 part 2

--- AoC-2023/01/Python/cke_aoc-2023-01.py
+++ AoC-2023/01/Python/cke_aoc-2023-01.py
@@ -1,16 +1,44 @@
-# read in the file "input.txt" and print out the first line
-with open("input.txt") as file:
-    calibration = []
-    for line in file:
+def change_char_to_digit(line):
+    char_to_digit = {
+        'zero': '0',
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9'
+    }
+    digits = []
+    for i in range(len(line)):
+        for word, digit in char_to_digit.items():
+            if line[i:].startswith(word):
+                digits.append(digit)
+        if line[i].isdigit():
+            digits.append(line[i])
+    return ''.join(digits)
 
-        for char in line:
-            if char.isdigit():
-                first = char
-                break
-        for char in reversed(line):
-            if char.isdigit():
-                last = char
-                break
-        calibration.append(first + last)
+def main():
+    # read in the file "input.txt" and print out the first line
+    with open("input.txt") as file:
+        calibration = []
+        for line in file:
+            line = change_char_to_digit(line)
+            print(line)
+            for char in line:
+                if char.isdigit():
+                    first = char
+                    break
+            for char in reversed(line):
+                if char.isdigit():
+                    last = char
+                    break
+            print(f"Calibration: {first} + {last}")
+            calibration.append(first + last)
 
-print(f"Sum of calibrations: {sum(map(int, calibration))}")
+    print(f"Sum of calibrations: {sum(map(int, calibration))}")
+
+if __name__ == "__main__":
+    main()
