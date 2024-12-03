f8679b9cc7acaf330a67514cf02e8f6faac6e7a8
gvtsch <kempkes.christoph@gmail.com>
2024-12-03 06:05:15 +0100

    Day 2 Part 2

--- AoC-2024/02/Python/cke_aoc-2024-02.py
+++ AoC-2024/02/Python/cke_aoc-2024-02.py
@@ -1,31 +1,36 @@
-
-def check_monotonic(sequence):
+def is_monotonic(sequence):
     is_increasing = all(sequence[i] <= sequence[i+1] for i in range(len(sequence)-1))
     is_decreasing = all(sequence[i] >= sequence[i+1] for i in range(len(sequence)-1))
     return is_increasing or is_decreasing
 
-def check_distance(sequence):
+def is_distance_valid(sequence):
     distances = [abs(int(sequence[i]) - int(sequence[i+1])) for i in range(len(sequence)-1)]
-    if all((distance <= 3 and distance >= 1) for distance in distances):
-        return True
+    return all((distance <= 3 and distance >= 1) for distance in distances)
 
+def check_sequence_with_one_removal(sequence):
+    if is_monotonic(sequence) and is_distance_valid(sequence):
+        return True
+        
+    for i in range(len(sequence)):
+        modified_sequence = sequence[:i] + sequence[i+1:] # Das Element an der Stelle i wird entfernt und quasi jede Kombination wird getestet
+        if is_monotonic(modified_sequence) and is_distance_valid(modified_sequence):
+            return True
+            
+    return False
 
 def main():
     print("Hello, Advent of Code 2024 - Day 2!")
     
     with open("../input.txt", 'r') as file:
         lines = file.readlines()
-
         safe_reports_number = 0
 
         for line in lines:
             line = list(map(int, line.split()))
-            if check_monotonic(line):
-                if check_distance(line):
-                    safe_reports_number += 1
+            if check_sequence_with_one_removal(line):
+                safe_reports_number += 1
                 
         print(safe_reports_number)    
-        
 
 if __name__ == "__main__":
     main()
